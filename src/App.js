5d9bcfa722fdec55f3d0b6f72f904aa532e00829
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2024-07-26 11:41:06 +0500

    first-v

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [number, setNumber] = useState(0);
+
+	const boxStyle = {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		flexDirection: "column",
+		height: "100vh",
+	};
+
+	const p = {
+		fontSize: "2rem",
+		color: "Blue",
+	};
+
+	function increment() {
+		setNumber(number + 1);
+	}
+
+	function decrement() {
+		if (number > 0) {
+			setNumber(number - 1);
+		}
+	}
+
+	return (
+		<div className="box" style={boxStyle}>
+			<p style={p}>{number}</p>
+			<div>
+				<button onClick={increment}>Increment</button>
+				<button onClick={decrement}>Decrement</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
